c756c834f07b5372892cca18d486b26cad23bc96
Nico Intelangelo <nicointelangelo1@gmail.com>
2022-10-12 21:50:54 -0300

    creacion empleado/empleados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadoComponent } from './components/empleado/empleado.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoComponent,
+    EmpleadosComponent,
   ],
   imports: [
     BrowserModule
